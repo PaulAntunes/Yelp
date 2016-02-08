492cfc4c19fcc72c449e0ae0c620f30fafee61e1
giajo090 <giajo090@gmail.com>
2016-02-08 13:51:12 +0100

    Css+content

---
+++ html/accueil.php
@@ -0,0 +1,7 @@
+<?php		<div id="yelpImg">Yelp ME</div>
+			<div><i>nom du lieu</i></div>
+			<div>note(moyenne)</div>
+			<div>Description</div>
+			<div>Infos concernant le lieu</div>
+			<div><iframe>Maps</iframe></div>
+			?>
