652980884a2d18fd07f12676cacdb7c5b03723c5
PaulAntunes <paulantunes@hotmail.fr>
2016-02-08 10:32:21 +0100

    dossier yelp
    contient maquettes;header etc

---
+++ img/sqelet.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Avis (
+  idAvis INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  buisness_idbuisness INTEGER UNSIGNED NOT NULL,
+  utilisateur_idUtilisateur INTEGER UNSIGNED NOT NULL,
+  txtAvis TEXT NULL,
+  dateAvis DATETIME NULL,
+  noteAvis TINYINT UNSIGNED NULL,
+  PRIMARY KEY(idAvis),
+  INDEX Avis_FKIndex1(buisness_idbuisness)
+);
+
+CREATE TABLE buisness (
+  idbuisness INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nomBuisness VARCHAR(20) NULL,
+  villeBuisness VARCHAR(70) NULL,
+  etoileBuisness TINYINT UNSIGNED NULL,
+  infosBuisness VARCHAR(200) NULL,
+  codepostBuisness TINYINT UNSIGNED NULL,
+  adresseBuisness VARCHAR(50) NULL,
+  PRIMARY KEY(idbuisness)
+);
+
+CREATE TABLE utilisateur (
+  idUtilisateur INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  mailUtilisateur VARCHAR(50) NULL,
+  nomUtilisateur VARCHAR(30) NULL,
+  mdpUtilisateur VARCHAR(20) NULL,
+  PRIMARY KEY(idUtilisateur)
+);
+
+
