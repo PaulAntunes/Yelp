2dda56f2cb6f3fb521621d1a5f593dfa4759a554
PaulAntunes <paulantunes@hotmail.fr>
2016-02-09 11:11:44 +0100

    modif inscription.php

--- php/inscription.php
+++ php/inscription.php
@@ -1,33 +1,29 @@
 <?php
     // connexion dans la base de données
-    try{
-        $bdd = new PDO('mysql:host=192.168.201.77;dbname=yelp', '');
-    }
-    catch (Exception $e){
-        die('Erreur : ' . $e->getMessage());
-    }
-
+  $bdd = new PDO('mysql:host=192.168.201.77;dbname=yelp', '');
+ 
     $request = $bdd->query("SELECT mailUtilisateur FROM utilisateur WHERE mailUtilisateur = '$_POST[emailRegister]'");
     $membre = $request->fetch();
 
 
-    if(!empty($_POST['firstNameRegister']) AND   if(!empty($_POST['lastNameRegister']) AND !empty($_POST['passRegister']) AND !empty($_POST['emailRegister'])){ // si les variables ne sont pas vides
+    if(!empty($_POST['firstName']) AND   (!empty($_POST['lastName']) AND !empty($_POST['pass']) AND !empty($_POST['email'])){ // si les variables ne sont pas vides
     
-        if(($_POST['mailUtilisateur']==$membre['mailUtilisateur'])){
+        if(($_POST['emailRegister']==$membre['mailUtilisateur'])){
             echo "Exist"; //On 'retourne' la valeur Error au javascript si la connexion n'est pas bonne
         }
         else{
-            $firstName = mysql_real_escape_string($_POST['firstNameRegister']);
-            $lastName = mysql_real_escape_string($_POST['lastNameRegister']);
-            $pass = mysql_real_escape_string($_POST['passRegister']);
-            $email = mysql_real_escape_string($_POST['emailRegister']);
+            $firstNameRegister = mysql_real_escape_string($_POST['firstNameRegister']);
+            $lastNameRegister = mysql_real_escape_string($_POST['lastNameRegister']);
+            $passRegister = mysql_real_escape_string($_POST['passRegister']);
+            $emailRegister = mysql_real_escape_string($_POST['emailRegister']);
+            echo'123'
 
             // puis on entre les données en base de données :
-            $insertion = $bdd->exec('INSERT INTO utilisateur VALUES( mailUtilisateur, :prénomUtilisateur, :nomUtilisateur, :mdpUtilisateur) ');
-            $insertion->bindvalue(':emailRegister',$email, PDO::PARAM_INT);
-            $insertion->bindvalue(':firstNameRegister',$firstName, PDO::PARAM_INT);    
-            $insertion->bindvalue(':lastNameRegister',$lastName, PDO::PARAM_INT);   
-            $insertion->bindvalue(':passRegister',$pass, PDO::PARAM_INT);  
+            $insertion = $bdd->exec('INSERT INTO utilisateur VALUES( mailUtilisateur, prénomUtilisateur, nomUtilisateur, mdpUtilisateur)');
+            $insertion->bindvalue('mailUtilisateur',$emailRegister, PDO::PARAM_INT);
+            $insertion->bindvalue('prénomUtilisateur',$firstNameRegister, PDO::PARAM_INT);    
+            $insertion->bindvalue('nomUtilisateur',$lastNameRegister, PDO::PARAM_INT);   
+            $insertion->bindvalue('mdpUtilisateur',$passRegister, PDO::PARAM_INT);  
             $insertion->execute();
             ));
             echo "Success"; //On 'retourne' la valeur Succes au javascript si la connexion est bonne
@@ -38,4 +34,5 @@
         echo "Error"; //On 'retourne' la valeur Error au javascript si la connexion n'est pas bonne
     }
 }
+
 ?>
