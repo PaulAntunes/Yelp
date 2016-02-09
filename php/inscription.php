749d250d206a0fa0ced401c39665281bf431c961
PaulAntunes <paulantunes@hotmail.fr>
2016-02-09 10:09:56 +0100

    maj

--- php/inscription.php
+++ php/inscription.php
@@ -1,7 +1,7 @@
 <?php
     // connexion dans la base de données
     try{
-        $bdd = new PDO('mysql:host=192.168.201.77;dbname=yelp', 'webforce3');
+        $bdd = new PDO('mysql:host=192.168.201.77;dbname=yelp', '');
     }
     catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
@@ -23,11 +23,12 @@
             $email = mysql_real_escape_string($_POST['emailRegister']);
 
             // puis on entre les données en base de données :
-            $insertion = $bdd->prepare('INSERT INTO utilisateur VALUES("", :name, :pass, :email)');
-            $insertion->execute(array(
-                'nomUtilisateur' => $name,
-                'mdpUtilisateur' => $pass,
-                'mailUtilisateur' => $email
+            $insertion = $bdd->exec('INSERT INTO utilisateur VALUES( mailUtilisateur, :prénomUtilisateur, :nomUtilisateur, :mdpUtilisateur) ');
+            $insertion->bindvalue(':emailRegister',$email, PDO::PARAM_INT);
+            $insertion->bindvalue(':firstNameRegister',$firstName, PDO::PARAM_INT);    
+            $insertion->bindvalue(':lastNameRegister',$lastName, PDO::PARAM_INT);   
+            $insertion->bindvalue(':passRegister',$pass, PDO::PARAM_INT);  
+            $insertion->execute();
             ));
             echo "Success"; //On 'retourne' la valeur Succes au javascript si la connexion est bonne
         }
@@ -36,5 +37,5 @@
     else{
         echo "Error"; //On 'retourne' la valeur Error au javascript si la connexion n'est pas bonne
     }
-
+}
 ?>
