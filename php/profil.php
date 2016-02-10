c2f10b2af836882efa0d930a886a52eac4617a9c
giajo090 <giajo090@gmail.com>
2016-02-10 11:03:43 +0100

    css modif <li><footer> + page profil

---
+++ php/profil.php
@@ -0,0 +1,61 @@
+<?php 
+require'../html/header.html';
+?>
+<section>
+	<figure>
+		PhotoProfil
+	</figure>
+	<div>
+		<h2>Votre Nom</h2>
+		<ul>
+			<li>Adresse :</li>
+			<li>tel :</li>
+			<li>Etudes :</li>
+			<li>Plat(s) préféré(s) :</li>
+			<li>tournevis préféré :</li>
+		</ul>
+	</div>
+	<ul>
+		<div>
+		Téléchargez votre photo!
+		<input type="file" placeholder="Votre photo">
+		</div>
+		<a href="">Mes amis</a>
+		<a href="">Mettre à jour profil</a>
+	</ul>
+	<ul>
+		<a href="amis">amis</a>
+		<a href="nb d'avis">nb d'avis</a>
+		<a href="photos">photos</a>
+	</ul>
+</section>
+<section> 
+	<aside>
+		<h3>vos avis récents</h3>
+		<ul>
+			<li>avis</li>
+			<li>avis</li>
+			<li>avis</li>
+			<li>avis</li>
+			<li>avis</li>
+
+		</ul>
+	</aside>
+	<article>
+		<div>
+			<h2>nom business</h2>
+			<figure>
+				
+			</figure>
+			<div>
+				infos business
+			</div>
+			<iframe src=""></iframe>
+			<div>avis business</div>
+		</div>
+	</article>
+</section>
+
+<?php
+require'../html/footer.html';
+?>
